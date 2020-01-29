2e69a6f339377faad30b0bcd3f96379f12d6cb6c
Largeswan <augustinevana@gmail.com>
2020-01-29 18:46:36 +0800

    +！ 解数独 四数之和

---
+++ TestCpp/LeetCode/FourSum.cpp
@@ -0,0 +1,178 @@
+﻿/*
+给定一个包含 n 个整数的数组 nums 和一个目标值 target，判断 nums 中是否存在四个元素 a，b，c 和 d ，使得 a + b + c + d 的值与 target 相等？找出所有满足条件且不重复的四元组。
+
+注意：
+答案中不可以包含重复的四元组。
+
+示例：
+给定数组 nums = [1, 0, -1, 0, -2, 2]，和 target = 0。
+
+满足要求的四元组集合为：
+[
+  [-1,  0, 0, 1],
+  [-2, -1, 1, 2],
+  [-2,  0, 0, 2]
+]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/4sum
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class FourSum
+{
+public:
+	vector<vector<int>> fourSum( vector<int>& nums, int target ) 
+	{
+		vector<vector<int>> output;
+		int numSize = nums.size();
+		if( numSize <= 3 )
+			return output;
+
+		QuickSort( nums, 0, numSize - 1 );
+
+		int i1Size = numSize - 3;
+		int i2Size = numSize - 2;
+		int lastI1 = -1;
+		int lastI2 = -1;
+
+		for( int i1 = 0; i1 < i1Size; ++i1 )
+		{
+			if( lastI1 >= 0 && nums[i1] <= nums[lastI1] )
+				continue;
+			lastI1 = i1;
+
+			lastI2 = -1;
+			for( int i2 = i1 + 1; i2 < i2Size; ++i2 )
+			{
+				if( lastI2 >= 0 && nums[i2] <= nums[lastI2] )
+					continue;
+				lastI2 = i2;
+
+				int i3 = i2 + 1;
+				int i4 = numSize - 1;
+
+				while( true )
+				{
+					int sum = nums[i1] + nums[i2] + nums[i3] + nums[i4];
+					if( sum < target )
+					{
+						int curVal = nums[i3];
+						while( i3 < i4 - 1 && nums[i3] <= curVal )
+							++i3;
+
+						if ( nums[i3] <= curVal )
+							break;
+					}
+					else if( sum > target )
+					{
+						int curVal = nums[i4];
+						while( i4 > i3 + 1 && nums[i4] >= curVal )
+							--i4;
+
+						if( nums[i4] >= curVal )
+							break;
+					}
+					else
+					{
+						vector<int> oneRet;
+						oneRet.reserve( 4 );
+
+						oneRet.push_back( nums[i1] );
+						oneRet.push_back( nums[i2] );
+						oneRet.push_back( nums[i3] );
+						oneRet.push_back( nums[i4] );
+
+						output.push_back( oneRet );
+
+						int curi3 = nums[i3];
+						while( i3 < i4 - 1 && nums[i3] <= curi3 )
+							++i3;
+
+						int curi4 = nums[i4];
+						while( i4 > i3 + 1 && nums[i4] >= curi4 )
+							--i4;
+
+						if( nums[i3] <= curi3 && nums[i4] >= curi4 )
+							break;
+					}
+				}
+			}
+		}
+
+		return output;
+	}
+
+private:
+
+	void QuickSort( vector<int>& nums, int start, int end )
+	{
+		if( start == end )
+			return;
+
+		int key = start;
+		int keyVal = nums[start];
+		int head = start;
+		int tail = end;
+		while( head < tail )
+		{
+			while( tail > key && nums[tail] >= keyVal )
+			{
+				--tail;
+			}
+			if( tail > key )
+			{
+				swapValues( nums[key], nums[tail] );
+				key = tail;
+			}
+
+			while( head < key && nums[head] <= keyVal )
+			{
+				++head;
+			}
+			if( head < key )
+			{
+				swapValues( nums[key], nums[head] );
+				key = head;
+			}
+		}
+
+		if( start < key - 1 )
+			QuickSort( nums, start, key - 1 );
+
+		if( key + 1 < end )
+			QuickSort( nums, key + 1, end );
+	}
+
+	void swapValues( int &a, int &b )
+	{
+		int temp = a;
+		a = b;
+		b = temp;
+	}
+
+};
+
+//int main()
+//{
+//	//{ 5,1,7,-15,56,2,-7 };
+//	//{ -1, 0, 1, 2, -1, -4 };
+//	//{ -1,0,0,0,0,1 };
+//	//{ 5,1,7,-15,56,2,-7 };
+//	vector<int> input = { -1,-5,-5,-3,2,5,0,4 };
+//	FourSum test;
+//	vector<vector<int>> results = test.fourSum( input , -7 );
+//
+//	int numOfGroups = results.size();
+//	for( int i = 0; i < numOfGroups; ++i )
+//	{
+//		cout << results[i][0] << ' ' << results[i][1] << ' ' << results[i][2] << ' ' << results[i][3] << endl;
+//	}
+//
+//	system( "pause" );
+//	return 0;
+//}
--- TestCpp/LeetCode/SimpleSodoku.cpp
+++ TestCpp/LeetCode/SimpleSodoku.cpp
@@ -12,65 +12,588 @@
 
 #include <vector>
 #include <unordered_set>
+#include <map>
+#include <windows.h>
 #include <iostream>
+#include <assert.h>
 using namespace std;
+
+struct IntPoint
+{
+	IntPoint() 
+		: x( 0 ), y( 0 )
+	{}
+	IntPoint( int _x, int _y ) 
+		: x( _x ), y( _y )
+	{}
+
+	bool operator==( const IntPoint &other ) const 
+	{
+		return x == other.x && y == other.y;
+	}
+
+	bool operator!=( const IntPoint &other ) const
+	{
+		return !(*this == other);
+	}
+
+	bool operator<( const IntPoint &other ) const 
+	{
+		if( y == other.y )
+		{
+			return x < other.x;
+		}
+		else
+		{
+			return y < other.y;
+		}
+	}
+
+	int x;
+	int y;
+};
+
+struct EmptyCell
+{
+	EmptyCell()
+		: resultChar( '.' ), guessChar( '.' )
+	{
+		range.reserve( 9 );
+		for( int i = 0; i < 9; ++i )
+		{
+			range.emplace( static_cast<char>( 49 + i ) );
+		}
+	}
+
+	bool remove( char val )
+	{
+		return range.erase( val ) > 0;
+	}
+
+	void add( char val )
+	{
+		range.emplace( val );
+	}
+
+	bool isGuessed()
+	{
+		return guessChar != '.';
+	}
+
+	bool isEmpty() 
+	{
+		return range.empty();
+	}
+
+	int getProbRange()
+	{
+		if( resultChar != '.' || isGuessed() )
+			return 0;
+		else
+			return range.size();
+	}
+
+	char getValidChar()
+	{
+		assert( range.size() > 0 );
+		return *range.begin();
+	}
+	
+	char resultChar;
+	unordered_set<char> range;
+
+	//guess
+	char guessChar;
+};
+
+struct GuessData
+{
+	GuessData()
+		: GuessPos( -1, -1 ), GuessChar( '.' )
+	{}
+
+	IntPoint GuessPos;
+	char GuessChar;
+
+	map<IntPoint, vector<char>> ExcludeByGuess;
+};
 
 class SimpleSodoku 
 {
 public:
-	bool isValidSudoku( vector<vector<char>>& board ) 
+	bool isValidSudoku( vector<vector<char>>& board );
+
+	void solveSudoku( vector<vector<char>>& board ) 
 	{
-		unordered_set<char> rowSets[9];
-		unordered_set<char> colSets[9];
-		unordered_set<char> subSets[9];
+		baseBoard = board;
 
-		for( int y = 0; y < 9; ++y )
+		loadSudoku( board, needToFill );
+		numOfFilled = 0;
+		if( !fillSudoku() )
+			return;
+
+		curGuessLevel = 0;
+		GuessData newData;
+		GuessStack.reserve( 5 );
+		GuessStack.push_back( newData );
+
+		if( !doGuessRecursively() )
+			return;
+		
+		//vector<IntPoint> dummy;
+		//printSodoku( board, dummy );
+
+		for( auto iter = selectionRange.begin(); iter != selectionRange.end(); ++iter )
 		{
-			for( int x = 0; x < 9; ++x )
+			const IntPoint &key = iter->first;
+			EmptyCell &cell = iter->second;
+
+			board[key.y][key.x] = cell.isGuessed() ? cell.guessChar : cell.resultChar;
+		}
+		//isValidSudoku( board );
+	}
+
+private:
+
+	bool doGuessRecursively();
+
+	void loadSudoku( vector<vector<char>>& board, int &cellToFill );
+	bool fillSudoku( bool doPrint = false );
+	bool guessSudoku( const IntPoint &key, char guessChar , bool doPrint = false );
+	void fallbackGuess();
+	bool updateSelectRange( vector<IntPoint> &changedKeys );
+	void printSodoku( vector<vector<char>>& board, vector<IntPoint> &changedCells );
+
+private:
+	
+	vector<vector<char>> baseBoard;
+
+	map<IntPoint, EmptyCell> selectionRange;
+	int needToFill;
+	int numOfFilled;
+
+	//guess stack
+	int curGuessLevel;
+	vector<GuessData> GuessStack;
+};
+
+bool SimpleSodoku::isValidSudoku( vector<vector<char>>& board )
+{
+	unordered_set<char> rowSets[9];
+	unordered_set<char> colSets[9];
+	unordered_set<char> subSets[9];
+
+	for( int y = 0; y < 9; ++y )
+	{
+		for( int x = 0; x < 9; ++x )
+		{
+			char curChar = board[y][x];
+			if( curChar == '.' )
+				continue;
+
+			unordered_set<char>::const_iterator rowIter = rowSets[y].find( curChar );
+			if( rowIter != rowSets[y].end() )
 			{
-				char curChar = board[y][x];
-				if( curChar == '.' )
-					continue;
+				cout << "Error:(" << x << "," << y << ")" << endl;
+				return false;
+			}
+			rowSets[y].emplace( curChar );
 
-				unordered_set<char>::const_iterator rowIter = rowSets[y].find( curChar );
-				if( rowIter != rowSets[y].end() )
-					return false;
-				rowSets[y].emplace( curChar );
+			unordered_set<char>::const_iterator colIter = colSets[x].find( curChar );
+			if( colIter != colSets[x].end() )
+			{
+				cout << "Error:(" << x << "," << y << ")" << endl;
+				return false;
+			}
+			colSets[x].emplace( curChar );
 
-				unordered_set<char>::const_iterator colIter = colSets[x].find( curChar );
-				if( colIter != colSets[x].end() )
-					return false;
-				colSets[x].emplace( curChar );
+			int subId = ( y / 3 ) * 3 + ( x / 3 );
+			unordered_set<char>::const_iterator subIter = subSets[subId].find( curChar );
+			if( subIter != subSets[subId].end() )
+			{
+				cout << "Error:(" << x << "," << y << ")" << endl;
+				return false;
+			}
+			subSets[subId].emplace( curChar );
+		}
+	}
 
-				int subId = ( y / 3 ) * 3 + ( x / 3 );
-				unordered_set<char>::const_iterator subIter = subSets[subId].find( curChar );
-				if( subIter != subSets[subId].end() )
-					return false;
-				subSets[subId].emplace( curChar );
+	cout << "Correct!" << endl;
+	return true;
+}
+
+bool SimpleSodoku::doGuessRecursively()
+{
+	if( numOfFilled >= needToFill )
+		return true;
+
+	vector<IntPoint> candiKey;
+	vector<char> candiChar;
+	
+	for( auto iter = selectionRange.begin(); iter != selectionRange.end(); ++iter )
+	{
+		const IntPoint &key = iter->first;
+		EmptyCell &cell = iter->second;
+
+		int numOfValid = cell.getProbRange();
+		if( numOfValid <= 0 )
+			continue;
+
+		vector<char> removeChars;
+		for( auto iter2 = cell.range.begin(); iter2 != cell.range.end(); ++iter2 )
+		{
+			char tarChar = *iter2;
+			bool ret = guessSudoku( key, tarChar , false );
+			ret = ret && fillSudoku( false );
+			
+			if( ret )//成功的猜测
+			{
+				candiKey.push_back( key );
+				candiChar.push_back( tarChar );
+			}
+			else
+			{
+				removeChars.push_back( tarChar );
+			}
+			fallbackGuess();
+		}
+		
+		map<IntPoint, vector<char>> &excludeMap = GuessStack[curGuessLevel].ExcludeByGuess;
+		for( char val : removeChars )
+		{
+			if( cell.remove( val ) )
+				excludeMap[key].push_back( val );
+		}
+		if( cell.range.size() == 0 )
+		{
+			return false;
+		}
+	}
+
+	fillSudoku();
+
+	int numOfCandidates = candiKey.size();
+	if( numOfCandidates <= 0 )
+		return false;
+
+	for( int i = 0; i < numOfCandidates; ++i )
+	{
+		EmptyCell &cell = selectionRange[candiKey[i]];
+		if ( cell.resultChar != '.' )
+			continue;
+
+		bool ret = guessSudoku( candiKey[i], candiChar[i] );
+		ret = ret && fillSudoku();
+		if( !ret )
+		{
+			fallbackGuess();
+			continue;
+		}
+
+		//cout << "level" << curGuessLevel << '(' << i << '/' << numOfCandidates << ')' << ':';
+		//cout << numOfFilled << '/' << needToFill << endl;
+
+		if( doGuessRecursively() )
+			return true;
+		else
+		{
+			fallbackGuess();
+		}
+	}
+
+	return false;
+}
+
+void SimpleSodoku::loadSudoku( vector<vector<char>>& board, int &cellToFill )
+{
+	cellToFill = 0;
+	for( int y = 0; y < 9; ++y )
+	{
+		for( int x = 0; x < 9; ++x )
+		{
+			if( board[y][x] != '.' )
+				continue;
+
+			IntPoint key( x, y );
+			EmptyCell &cell = selectionRange[key];
+
+			int subStartX = ( x / 3 ) * 3;
+			int subStartY = ( y / 3 ) * 3;
+			for( int i = 0; i < 9; ++i )
+			{
+				char rowChar = board[y][i];
+				if( rowChar != '.' )
+				{
+					cell.remove( rowChar );
+				}
+
+				char colChar = board[i][x];
+				if( colChar != '.' )
+				{
+					cell.remove( colChar );
+				}
+
+				int subX = subStartX + ( i % 3 );
+				int subY = subStartY + ( i / 3 );
+				char subChar = board[subY][subX];
+				if( subChar != '.' )
+				{
+					cell.remove( subChar );
+				}
+			}
+
+			++cellToFill;
+		}
+	}
+}
+
+bool SimpleSodoku::fillSudoku( bool doPrint )
+{
+	while( true )
+	{
+		//遍历所有未填写的格子
+		vector<IntPoint> changedKeys;
+		for( auto iter = selectionRange.begin(); iter != selectionRange.end(); ++iter )
+		{
+			const IntPoint &key = iter->first;
+			EmptyCell &cell = iter->second;
+			int numOfValid = cell.getProbRange();
+			if( numOfValid == 1 )//填写确定的格子
+			{
+				changedKeys.push_back( key );
+				cell.resultChar = *( cell.range.begin() );
+				++numOfFilled;
 			}
 		}
 
-		return true;
+		int numOfChanged = changedKeys.size();
+		if( numOfChanged <= 0 )
+		{
+			break;
+		}
+		
+		//改写其他格
+		bool ret = updateSelectRange( changedKeys );
+		if ( doPrint ) 
+			printSodoku( baseBoard, changedKeys );
+		if( !ret )
+			return false;//改写过程出错
 	}
-};
+
+	return true;
+}
+
+bool SimpleSodoku::guessSudoku( const IntPoint &key , char guessChar , bool doPrint )
+{
+	//随便选一个（选第一个有效的）
+	EmptyCell &cell = selectionRange[key];
+	cell.guessChar = guessChar;
+	++numOfFilled;
+	
+	++curGuessLevel;
+	GuessData newData;
+	newData.GuessChar = guessChar;
+	newData.GuessPos = key;
+	GuessStack.push_back( newData );
+
+	//改写其他格
+	vector<IntPoint> keyList;
+	keyList.push_back( key );
+	
+	bool ret = updateSelectRange( keyList );
+	if( doPrint )
+		printSodoku( baseBoard, keyList );
+	return ret;
+}
+
+void SimpleSodoku::fallbackGuess()
+{
+	//回退猜想
+	IntPoint &guessPos = GuessStack[curGuessLevel].GuessPos;
+	selectionRange[guessPos].guessChar = '.';
+	--numOfFilled;
+
+	//回退其他格
+	map<IntPoint, vector<char>> &excludeMap = GuessStack[curGuessLevel].ExcludeByGuess;
+	for( auto Iter1 = excludeMap.begin(); Iter1 != excludeMap.end(); ++Iter1 )
+	{
+		const IntPoint &key = Iter1->first;
+		EmptyCell &cell = selectionRange[key];
+
+		vector<char> &charlist = Iter1->second;
+		for( auto Iter2 = charlist.begin(); Iter2 != charlist.end(); ++Iter2 )
+		{
+			cell.add( *Iter2 );
+		}
+		if( cell.resultChar != '.' )
+		{
+			--numOfFilled;
+			cell.resultChar = '.';
+		}
+	}
+	
+	GuessStack.pop_back();
+	--curGuessLevel;
+}
+
+bool SimpleSodoku::updateSelectRange( vector<IntPoint> &changedKeys )
+{
+	bool result = true;
+	map<IntPoint, vector<char>> &excludeMap = GuessStack[curGuessLevel].ExcludeByGuess;
+
+	//填写之后 其他格子可能性改变
+	int numOfChanged = changedKeys.size();
+	for( int i = 0; i < numOfChanged; ++i )
+	{
+		IntPoint &key = changedKeys[i];
+		EmptyCell &cell = selectionRange[key];
+		char curChar = cell.isGuessed() ? cell.guessChar : cell.resultChar;
+
+		//改写 同行 同列 同九宫的
+		int subStartX = ( key.x / 3 ) * 3;
+		int subStartY = ( key.y / 3 ) * 3;
+		for( int k = 0; k < 9; ++k )
+		{
+			IntPoint rowKey( k, key.y );
+			auto rowIter = selectionRange.find( rowKey );
+			if( k != key.x  && rowIter != selectionRange.end() )
+			{
+				EmptyCell &rowCell = rowIter->second;
+				if( rowCell.guessChar == curChar || rowCell.resultChar == curChar )
+				{
+					//cout << "Error:(" << rowKey.x << "," << rowKey.y << ")" << endl;
+					//result = false;//猜测是错误的
+					return false;
+				}
+				else if ( rowCell.getProbRange() > 0 )
+				{
+					if( rowCell.remove( curChar ) )
+						excludeMap[rowKey].push_back( curChar );
+				}
+			}
+
+			IntPoint colKey( key.x, k );
+			auto colIter = selectionRange.find( colKey );
+			if( k != key.y && colIter != selectionRange.end() )
+			{
+				EmptyCell &colCell = colIter->second;
+				if( colCell.guessChar == curChar || colCell.resultChar == curChar )
+				{
+					//cout << "Error:(" << colKey.x << "," << colKey.y << ")" << endl;
+					//result = false;//猜测是错误的
+					return false;
+				}
+				else if( colCell.getProbRange() > 0 )
+				{
+					if( colCell.remove( curChar ) )
+						excludeMap[colKey].push_back( curChar );
+				}
+			}
+
+			IntPoint subKey( subStartX + ( k % 3 ), subStartY + ( k / 3 ) );
+			auto subIter = selectionRange.find( subKey );
+			if( subKey != key && subIter != selectionRange.end() )
+			{
+				EmptyCell &subCell = subIter->second;
+				if( subCell.guessChar == curChar || subCell.resultChar == curChar )
+				{
+					//cout << "Error:(" << subKey.x << "," << subKey.y << ")" << endl;
+					//result = false;//猜测是错误的
+					return false;
+				}
+				else if( subCell.getProbRange() > 0 )
+				{
+					if( subCell.remove( curChar ) )
+						excludeMap[subKey].push_back( curChar );
+				}
+			}
+		}
+	}
+
+	return true;
+}
+
+void SimpleSodoku::printSodoku( vector<vector<char>>& board , vector<IntPoint> &changedCells )
+{
+	cout << "======================" << endl << endl;
+
+	for( int y = 0; y < 9; ++y )
+	{
+		for( int x = 0; x < 9; ++x )
+		{
+			IntPoint key( x, y );
+
+			if( board[y][x] != '.' )
+			{
+				cout << board[y][x] << ' ';
+			}
+			else if( selectionRange.find( key ) != selectionRange.end() )
+			{
+				EmptyCell &cell = selectionRange[key];
+
+				unsigned short color = 0;
+				int num = changedCells.size();
+				for( int i = 0; i < num; ++i )
+				{
+					if( key == changedCells.at( i ) )
+					{
+						color = cell.isGuessed() ? FOREGROUND_GREEN : FOREGROUND_RED;
+						break;
+					}
+				}
+				
+				if( color > 0 )
+				{
+					SetConsoleTextAttribute( GetStdHandle( STD_OUTPUT_HANDLE ), color );
+				}
+
+				if ( cell.resultChar != '.' )
+					cout << cell.resultChar << ' ';
+				else if ( cell.isGuessed() )
+					cout << cell.guessChar << ' ';
+				else
+					cout << ". ";
+
+				if( color > 0 )
+				{
+					SetConsoleTextAttribute( GetStdHandle( STD_OUTPUT_HANDLE ), FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE );
+				}
+			}
+
+			if( x == 2 || x == 5 )
+			{
+				cout << '|';
+			}
+		}
+		cout << endl;
+
+		if( y == 2 || y == 5 )
+		{
+			cout << "-------------------" << endl;
+		}
+	}
+	cout << endl;
+}
 
 int main()
 {
-	vector<vector<char>> input = 
-	{
-		{'5','3','.','.','7','.','.','.','.'},
-		{'6','.','.','1','9','5','.','6','.'},
-		{'.','9','8','.','.','.','.','6','.'},
-		{'8','.','.','.','6','.','.','.','3'},
-		{'4','.','.','8','.','3','.','.','1'},
-		{'7','.','.','.','2','.','.','.','6'},
-		{'.','6','.','.','.','.','2','8','.'},
-		{'.','.','.','4','1','9','.','.','5'},
-		{'.','.','.','.','8','.','.','7','9'}
+	vector<vector<char>> input =
+	{		
+		{'.','1','.','4','.','.','.','5','.'},
+		{'7','.','.','.','.','6','.','.','.'},
+		{'.','.','.','.','.','.','.','.','.'},
+		{'.','4','.','1','5','.','.','3','.'},
+		{'.','.','.','.','.','.','.','.','2'},
+		{'.','.','.','9','.','.','.','.','6'},
+		{'.','.','.','.','.','.','9','1','.'},
+		{'.','.','.','.','.','.','.','.','.'},
+		{'6','.','2','.','.','7','.','.','.'}
 	};
 
 	SimpleSodoku test;
-	cout << test.isValidSudoku( input ) << endl;
+
+	test.solveSudoku( input );
+	//test.isValidSudoku( input );
 
 	system( "pause" );
 	return 0;
